81dca79ffd5d3b30f451f66c5eea09a570222bba
unknown <michnbruno@gmail.com>
2022-07-05 21:07:30 -0400

    cors

--- src/main/java/com/bezkoder/spring/mssql/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/mssql/controller/TutorialController.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.mssql.model.Tutorial;
 import com.bezkoder.spring.mssql.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+// @CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
